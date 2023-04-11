0ed4016c118d5e8d612e408c3e50b6c36e3577b9
Hawawou <hawawout2000@gmail.com>
2023-04-11 15:59:19 +0100

    added no sandbox

--- test.py
+++ test.py
@@ -3,6 +3,11 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument('--no-sandbox')
+
 def typing(element, text):
     for i in text:
         element.send_keys(i)
