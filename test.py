b203914e78faf0af509a64130b95717888d479d0
Hawawou <hawawout2000@gmail.com>
2023-04-12 12:17:31 +0100

    changes

--- test.py
+++ test.py
@@ -5,11 +5,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
-chrome_options.add_argument('–headless')
-chrome_options.add_argument("--remote-debugging-port=9222")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("headless")
+chrome_options.add_argument("window-size=1280x800")
+chrome_options.add_argument("no-sandbox")
+chrome_options.add_argument("–disable-dev-shm-usage")
 chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-setuid-sandbox")
 chrome_options.add_argument("disable-infobars")
 
 
