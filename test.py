6f245c0ead2dfe0579a52512f601ddb74b7e1767
Hawawou <hawawout2000@gmail.com>
2023-04-12 12:38:08 +0100

    change

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("headless")
 chrome_options.add_argument("window-size=1280x800")
-chrome_options.add_argument("no-sandbox")
 chrome_options.add_argument("–disable-dev-shm-usage")
 chrome_options.add_argument("start-maximized")
 chrome_options.add_argument("--disable-gpu")
