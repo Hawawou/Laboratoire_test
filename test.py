11dc18cd6dd3ead0cbae01b0f814a7582244f3f4
Hawawou <hawawout2000@gmail.com>
2023-04-12 10:40:35 +0100

    removed no sandbox

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('–headless')
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument('--disable-dev-shm-usage')
@@ -67,7 +66,7 @@ class LaboratoireTest(unittest.TestCase):
     #     typing(phone, "672345892")
     #     time.sleep()
 
-
+      
         driver.close()
     
 
